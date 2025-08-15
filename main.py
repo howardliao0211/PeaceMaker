6e06d23f571a521a0e7e86fe725f2555092cc56d
hlia0034 <hlia0034@student.monash.edu>
2025-08-15 14:34:34 +1000

    pushed current backend and html

---
+++ main.py
@@ -0,0 +1,14 @@
+from fastrtc import Stream
+import numpy as np
+
+
+def flip_vertically(image):
+    return np.flip(image, axis=0)
+
+stream = Stream(
+    handler=flip_vertically,
+    modality="video",
+    mode="send-receive",
+)
+
+stream.ui.launch()
